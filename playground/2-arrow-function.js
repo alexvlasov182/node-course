c83535802ce0df068c125a1db22950eb9e1c78cb
Oleksandr Vlasov <drumlife182@gmail.com>
2025-03-28 20:41:10 +0100

    ES6 Aside: Arrow Functions

--- playground/2-arrow-function.js
+++ playground/2-arrow-function.js
@@ -3,3 +3,16 @@
 // }
 const square = (x) => x * x;
 console.log(square(9))
+
+const event = {
+  name: 'Birthday Party!!!!',
+  guestList: ["Sasha", 'Nastya', 'Erik', "Alex", "Majara"],
+  printGuestList() {
+    console.log('Guest list for ' + this.name)
+    this.guestList.forEach((guest) => {
+      console.log(guest + ' is attending ' + this.name)
+    })
+  }
+}
+
+event.printGuestList()
