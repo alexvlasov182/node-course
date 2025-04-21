5b70b8b471017346a738099e43965a83ee8066e1
Oleksandr Vlasov <drumlife182@gmail.com>
2025-04-21 18:53:29 +0200

    deleted debugging

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -6,9 +6,6 @@ const addNote = (title, body) => {
   
   const duplicateNote = notes.find((note) => note.title === title);
 
-
-  debugger
-
   if (!duplicateNote) {
     notes.push({
       title: title,
