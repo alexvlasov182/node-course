2300c94aeef1a20828b18c1f3f4d5586774238f2
Oleksandr Vlasov <drumlife182@gmail.com>
2025-04-26 22:13:06 +0200

    deltedte testing log

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -14,7 +14,6 @@ const addNote = (title, body) => {
 
     saveNotes(notes);
     console.log(chalk.green.inverse("New note added!"));
-
   } else {
     console.log(chalk.red.inverse("Note title taken!"));
   }
