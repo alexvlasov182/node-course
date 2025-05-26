71b7d659c58c8613cba63680c5fe4cabd1b3a728
Oleksandr Vlasov <drumlife182@gmail.com>
2025-05-26 22:00:47 +0200

    Asynchronous Basics

---
+++ weather-app/app.js
@@ -0,0 +1,13 @@
+console.log("Starting Weather App is running...");
+
+setTimeout(() => {
+  console.log("2 second timer completed, fetching weather data...");
+}
+, 2000);
+
+setTimeout(() => {
+  console.log("0 Second timer completed, fetching weather data...");
+}
+, 0);
+
+console.log("Stopping");
