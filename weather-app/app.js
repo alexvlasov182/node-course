2ace8af8c0009ec15caaa3f536cd50d398ffffec
Oleksandr Vlasov <drumlife182@gmail.com>
2025-08-02 18:27:07 +0200

    feat: customized HTTP request

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,8 @@
 const request = require("postman-request");
 
-const url = "https://api.weatherstack.com/current?access_key=2ac5c82a4d477371f3fd8e11c9d534f5&query=48.1351,11.5820"
+const url = "https://api.weatherstack.com/current?access_key=2ac5c82a4d477371f3fd8e11c9d534f5&query=48.1351,11.5820&units=m";
 
 
-request({url: url}, (error, response) => {
-  const data = JSON.parse(response.body)
-  console.log(data);
-})
+request({url: url, json: true}, (error, response) => {
+  console.log(response.body.current.weather_descriptions[0] + ': It is currently ' + response.body.current.temperature + ' degrees out in Munich. There is a ' + response.body.current.feelslike + ' degrees feels like temperature.');
+}) 
