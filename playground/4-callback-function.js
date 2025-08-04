0586b5c02b8179b1a125c9c3d23eb9092645646d
Oleksandr Vlasov <drumlife182@gmail.com>
2025-08-04 09:07:12 +0200

    docs: the callback function

---
+++ playground/4-callback-function.js
@@ -0,0 +1,30 @@
+// setTimeout(() => {
+//   console.log("Two seconds have passed since the last log.");
+// }, 2000)
+
+// const geocode = (address, callback) => {
+//   setTimeout(() => {
+//     const data = {
+//       latitude: 0,
+//       longitude: 0
+//     }
+//     callback(data)
+//   }, 2000)
+// }
+
+// geocode("Philadelphia", (data) => {
+//   console.log(data);
+// });
+
+
+
+const add = (a,b, callback) => {
+  setTimeout(() => {
+    const sum = a + b;
+    callback(sum);
+  }, 2000)
+}
+
+add(1,9, (sum) => {
+  console.log(sum);
+})
