6539fcf85be976c85dff5d18fca5fe78d5117fc4
Oleksandr Vlasov <drumlife182@gmail.com>
2025-08-05 13:44:45 +0200

    docs: callback abstraction

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,36 +1,19 @@
-const request = require("postman-request");
-const geocode = require('./utils/geocode')
-// const url = "https://api.weatherstack.com/current?access_key=2ac5c82a4d477371f3fd8e11c9d534f5&query=48.1351,11.5820&units=m";
+// app.js
+require('dotenv').config();
 
-// request({url: url, json: true}, (error, response) => {
-//   if (error) {
-//     console.log('Unable to connect to weather service!');
-//   } else if (response.body.error) {
-//     console.log('Unable to find location. Try another search.');
-//   } else {
-//     console.log(response.body.current.weather_descriptions[0] + ': It is currently ' + response.body.current.temperature + ' degrees out in Munich. There is a ' + response.body.current.feelslike + ' degrees feels like temperature.');
-//   }
-// }) 
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
-// Gecoding
-// const geocodeUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/Munich.json?access_token=pk.eyJ1IjoiZHJ1bWxpZmUxODIiLCJhIjoiY21kdnF6MmJtMHdobjJscXdrbXBhcHcwcCJ9.BlyWpKK3Crmh3Ydl7XXeIw&limit=1";
+const query = process.argv[2] || 'Boston';
 
-// request({url: geocodeUrl, json: true}, (error, response) => {
-//   if (error) {
-//     console.log('Unable to connect to location service!');
-//   } else if (response.body.features.length === 0) {
-//     console.log('Unable to find location. Try another search.');
-//   } else {
-//     const latitude = response.body.features[0].geometry.coordinates[1];
-//     const longitude = response.body.features[0].geometry.coordinates[0];
-//     console.log('Latitude:', latitude);
-//     console.log('Longitude:', longitude);
-//   }
-// });
-
-
-
-geocode('Boston', (error, data) => {
-  console.log('Error', error)
-  console.log("Data", data)
-})
+(async () => {
+  try {
+    const geo = await geocode(query);
+    console.log('Location:', geo.location);
+    const weather = await forecast(geo.latitude, geo.longitude);
+    const forecastText = typeof weather === 'object' && weather.text ? weather.text : weather;
+    console.log('Forecast:', forecastText);
+  } catch (err) {
+    console.log('Error:', err);
+  }
+})();
--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -1,21 +1,58 @@
-const request = require("postman-request");
+
+const request = require('postman-request');
+
+const MAPBOX_TOKEN = process.env.MAPBOX_TOKEN;
 
 const geocode = (address, callback) => {
-  const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiZHJ1bWxpZmUxODIiLCJhIjoiY21kdnF6MmJtMHdobjJscXdrbXBhcHcwcCJ9.BlyWpKK3Crmh3Ydl7XXeIw&limit=1'
+  if (!address) {
+    const err = 'Address must be provided';
+    if (typeof callback === 'function') return callback(err, undefined);
+    return Promise.reject(err);
+  }
 
-  request({url: url,json: true}, (error, response) => {
-    if (error) {
-      callback('Unable to connect to location services!', undefined)
-    } else if (response.body.features.length === 0) {
-      callback('Unable to find location. Try another search.', undefined)
-    } else {
-      callback(undefined, {
-        latitude: response.body.features[0].center[1],
-        longitude: response.body.features[0].center[0],
-        location: response.body.features[0].place_name
-      })
-    }
-  })
-}
+  if (!MAPBOX_TOKEN) {
+    const err = 'Missing MAPBOX_TOKEN in environment';
+    if (typeof callback === 'function') return callback(err, undefined);
+    return Promise.reject(err);
+  }
+
+  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+    address
+  )}.json?access_token=${MAPBOX_TOKEN}&limit=1`;
+
+  const doRequest = () =>
+    new Promise((resolve, reject) => {
+      request({ url, json: true, timeout: 5000 }, (error, response) => {
+        if (error) return reject('Unable to connect to location services!');
+        const body = response && response.body;
+
+        if (process.env.DEBUG_GEOCODE === 'true') {
+
+          console.log('DEBUG Mapbox response:', JSON.stringify(body, null, 2));
+        }
+
+        if (!body) return reject('No response from location service.');
+        if (!Array.isArray(body.features) || body.features.length === 0) {
+          return reject('Unable to find location. Try another search.');
+        }
+
+        const place = body.features[0];
+        const result = {
+          latitude: place.center[1],
+          longitude: place.center[0],
+          location: place.place_name
+        };
+        resolve(result);
+      });
+    });
+
+  if (typeof callback === 'function') {
+    doRequest()
+      .then((res) => callback(undefined, res))
+      .catch((err) => callback(err, undefined));
+  } else {
+    return doRequest();
+  }
+};
 
 module.exports = geocode;
