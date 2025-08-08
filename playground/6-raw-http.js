7bb4ac6ae938f26dca8d0fd4bcb93eba89aab965
Oleksandr Vlasov <drumlife182@gmail.com>
2025-08-08 11:26:03 +0200

    docs: http request withou library

---
+++ playground/6-raw-http.js
@@ -0,0 +1,24 @@
+const http = require("http");
+const {weatherstackKey} = require('../weather-app/config')
+
+const url = `http://api.weatherstack.com/current?access_key=${weatherstackKey}&query=45,-75&units=m`
+
+const request = http.request(url, (response) => {
+
+  let data = '';
+  response.on('data', (chunk) => {
+    data = data + chunk.toString()
+    console.log(chunk)
+  })
+
+  response.on('end', () => {
+    const body = JSON.parse(data)
+    console.log(body);
+  })
+})
+
+request.on('error', (error) => {
+  console.log('An error', error);
+});
+
+request.end();
