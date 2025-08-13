aef964bafd096b241906f181da15513566eb6a7f
Oleksandr Vlasov <drumlife182@gmail.com>
2025-08-13 09:09:16 +0200

    docs: serving up static assets

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,19 +4,30 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
-  res.send('Express & Node.js')
+  res.send('<h1>Weather</h1>')
 })
 
 app.get('/help', (req, res) => {
-  res.send('Help Page')
+  res.send({
+    name: "John Doe",
+    age: 30
+  })
 })
 
 app.get('/about', (req, res) => {
-  res.send('This is about page')
+  res.send('<h1>My Weather App</h1>')
 })
 
 app.get('/weather', (req, res) => {
-  res.send('This weather page')
+  res.send([{
+    location: "New York",
+    temperature: 25,
+    description: "Sunny"
+  }, {
+    location: "Los Angeles",
+    temperature: 30,
+    description: "Clear"
+  }])
 })
 
 app.listen(port, () => {
